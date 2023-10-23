b5c3c58f4539c368e6c3d1cc355db9537b2199f7
Artem Kulikov <chigar2010@yandex.ru>
2023-10-24 01:58:16 +0200

    rework after 1st review

--- ya_news/news/pytest_tests/test_logic.py
+++ ya_news/news/pytest_tests/test_logic.py
@@ -4,76 +4,134 @@ import pytest
 from django.urls import reverse
 from pytest_django.asserts import assertFormError, assertRedirects
 
-from news.forms import BAD_WORDS, WARNING
+from news.forms import WARNING
 from news.models import Comment
 
 
+DETAIL_URL = pytest.lazy_fixture('detail_url')
+LOGIN_URL = pytest.lazy_fixture('login_url')
+EDIT_URL = pytest.lazy_fixture('edit_url')
+DELETE_URL = pytest.lazy_fixture('delete_url')
+
+
 @pytest.mark.django_db
+@pytest.mark.parametrize('url', (DETAIL_URL,))
 def test_user_can_create_comment(
-    author, author_client, form_data, news, detail_url
+    author, author_client, form_data_for_comment, news, url
 ):
+    comments_before = author_client.get(
+        url
+    ).context['news'].comment_set.all()
     assertRedirects(
-        author_client.post(detail_url, data=form_data),
-        f'{detail_url}#comments'
+        author_client.post(url, data=form_data_for_comment),
+        f'{url}#comments'
     )
-    assert Comment.objects.count() == 1
-    comment = Comment.objects.get()
-    assert comment.text == form_data['text']
+    comments_after = author_client.get(
+        url
+    ).context['news'].comment_set.all()
+    created_comment = set(comments_after) - set(comments_before)
+    assert created_comment, 'No Comments!'
+    comment = Comment.objects.get(id=created_comment.pop().id)
+    assert comment.text == form_data_for_comment['text']
     assert comment.author == author
     assert comment.news == news
 
 
 @pytest.mark.django_db
-def test_anonymous_user_cant_create_comment(detail_url, client, form_data):
+@pytest.mark.parametrize(
+    'url, log_url',
+    ((DETAIL_URL, LOGIN_URL),)
+)
+def test_anonymous_user_cant_create_comment(
+    client, form_data_for_comment, url, log_url
+):
+    comments_before_post = client.get(url).context['news'].comment_set.all()
     assertRedirects(
-        client.post(detail_url, data=form_data),
-        f'{reverse("users:login")}?next={detail_url}'
+        client.post(url, data=form_data_for_comment),
+        f'{log_url}?next={url}'
     )
-    assert Comment.objects.count() == 0
+    comments_after_post = client.get(url).context['news'].comment_set.all()
+    assert set(comments_after_post) == set(comments_before_post)
 
 
 @pytest.mark.django_db
-def test_user_cant_use_bad_words(detail_url, admin_client):
+@pytest.mark.parametrize('url', (DETAIL_URL,))
+def test_user_cant_use_bad_words(
+    url, admin_client, form_data_for_comment_with_bad_word
+):
+    comments_before_post = admin_client.get(
+        url
+    ).context['news'].comment_set.all()
     assertFormError(
         admin_client.post(
-            detail_url,
-            data={'text': f'Text, {BAD_WORDS[0]}, etc'}
+            url,
+            data=form_data_for_comment_with_bad_word
         ),
         'form',
         'text',
         errors=WARNING
     )
-    assert Comment.objects.count() == 0
+    comments_after_post = admin_client.get(
+        url
+    ).context['news'].comment_set.all()
+    assert set(comments_after_post) == set(comments_before_post)
 
 
-def test_author_can_delete_comment(delete_url, author_client, detail_url):
-    assertRedirects(author_client.delete(delete_url), detail_url + '#comments')
-    assert Comment.objects.count() == 0
+@pytest.mark.parametrize('del_url, det_url', ((DELETE_URL, DETAIL_URL), ))
+def test_author_can_delete_comment(del_url, author_client, det_url):
+    comments_before_del = author_client.get(
+        det_url
+    ).context['news'].comment_set.all()
+    assertRedirects(author_client.delete(del_url), det_url + '#comments')
+    comments_after_del = author_client.get(
+        det_url
+    ).context['news'].comment_set.all()
+    assert set(comments_before_del) - set(comments_after_del)
 
 
+@pytest.mark.parametrize('del_url, det_url', ((DELETE_URL, DETAIL_URL), ))
 def test_user_cant_delete_comment_of_another_user(
-    delete_url, admin_client
+    del_url, admin_client, det_url, comment
 ):
-    assert admin_client.delete(delete_url).status_code == HTTPStatus.NOT_FOUND
-    assert Comment.objects.count() == 1
+    comments_before_del = admin_client.get(
+        det_url
+    ).context['news'].comment_set.all()
+    assert admin_client.delete(del_url).status_code == HTTPStatus.NOT_FOUND
+    comments_after_del = admin_client.get(
+        det_url
+    ).context['news'].comment_set.all()
+    assert set(comments_after_del) == set(comments_before_del)
+    comment_from_db = Comment.objects.get(id=comment.id)
+    assert comment_from_db.news == comment.news
+    assert comment_from_db.author == comment.author
+    assert comment_from_db.text == comment.text
+    assert comment_from_db.created == comment.created
 
 
+@pytest.mark.parametrize('ed_url, det_url', ((EDIT_URL, DETAIL_URL), ))
 def test_author_can_edit_comment(
-    edit_url, author_client, form_data, detail_url, comment
+    ed_url, author_client, form_data_for_comment,
+    det_url, comment, author, news
 ):
     assertRedirects(
-        author_client.post(edit_url, data=form_data),
-        detail_url + '#comments'
+        author_client.post(ed_url, data=form_data_for_comment),
+        det_url + '#comments'
     )
     comment.refresh_from_db()
-    assert comment.text == form_data['text']
+    assert comment.text == form_data_for_comment['text']
+    assert comment.author == author
+    assert comment.news == news
 
 
+@pytest.mark.parametrize('ed_url', (EDIT_URL, ))
 def test_user_cant_edit_comment_of_another_user(
-    edit_url, admin_client, form_data, comment
+    ed_url, admin_client, form_data_for_comment, comment
 ):
     assert admin_client.post(
-        edit_url, data=form_data
+        ed_url, data=form_data_for_comment
     ).status_code == HTTPStatus.NOT_FOUND
     comment_from_db = Comment.objects.get(id=comment.id)
+    assert comment_from_db.news == comment.news
+    assert comment_from_db.author == comment.author
     assert comment_from_db.text == comment.text
+    assert comment_from_db.created == comment.created
