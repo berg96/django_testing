1b7fa4847b336773be3646f98d389361cca2e016
Artem Kulikov <chigar2010@yandex.ru>
2023-10-25 00:21:45 +0200

    rework after 1st review

--- ya_note/notes/tests/test_content.py
+++ ya_note/notes/tests/test_content.py
@@ -1,14 +1,25 @@
-from django.test import TestCase
+from django.test import Client, TestCase
 from django.urls import reverse
 
+from notes.forms import NoteForm
 from notes.models import Note, User
+
+
+NOTE_SLUG_FOR_TEST = 'test_slug'
+LIST_URL = reverse('notes:list')
+ADD_URL = reverse('notes:add')
+EDIT_URL = reverse('notes:edit', args=(NOTE_SLUG_FOR_TEST,))
 
 
 class TestContent(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.author = User.objects.create(username='IamGroot')
+        cls.author_client = Client()
+        cls.author_client.force_login(cls.author)
         cls.reader = User.objects.create(username='IceFrog')
+        cls.reader_client = Client()
+        cls.reader_client.force_login(cls.reader)
         Note.objects.bulk_create(
             Note(
                 title=f'Note{index}',
@@ -18,40 +29,35 @@ class TestContent(TestCase):
             )
             for index in range(2)
         )
-        cls.the_note = Note.objects.create(
-            title='the_title',
-            text='the_text',
+        cls.note = Note.objects.create(
+            title='Test title',
+            text='Test text',
             author=cls.author,
+            slug=NOTE_SLUG_FOR_TEST,
         )
 
     def test_notes_count(self):
-        self.client.force_login(self.author)
         self.assertEqual(
-            len(self.client.get(reverse('notes:list')).context['object_list']),
+            len(self.author_client.get(LIST_URL).context['object_list']),
             Note.objects.count()
         )
 
     def test_notes_list_for_different_users(self):
-        users_availability = (
-            (self.author, True),
-            (self.reader, False)
+        clients_availability = (
+            (self.author_client, True),
+            (self.reader_client, False)
         )
-        for user, availability in users_availability:
-            self.client.force_login(user)
-            with self.subTest(user=user):
+        for parametrized_client, availability in clients_availability:
+            with self.subTest(client=parametrized_client):
                 self.assertEqual(
-                    (self.the_note in self.client.get(
-                        reverse('notes:list')
+                    (self.note in parametrized_client.get(
+                        LIST_URL
                     ).context['object_list']),
                     availability
                 )
 
     def test_pages_contains_form(self):
-        for name, args in (
-            ('notes:add', None),
-            ('notes:edit', (self.the_note.slug,))
-        ):
-            self.client.force_login(self.author)
-            self.assertIn(
-                'form', self.client.get(reverse(name, args=args)).context
-            )
+        for url in (ADD_URL, EDIT_URL):
+            context = self.author_client.get(url).context
+            self.assertIn('form', context)
+            self.assertIsInstance(context['form'], NoteForm)
